119952aebc9602ffdfbe26f17aee8b722de9444e
yankeeq <125729776+yankeeq@users.noreply.github.com>
2023-03-15 16:42:26 +0300

    Задичи добавил 4 и семинар 5

---
+++ home4/Program.cs
@@ -0,0 +1,77 @@
+﻿// // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// // 3, 5 -> 243 (3⁵)
+// // 2, 4 -> 16
+/*
+Console.Write("напишите число :");
+int number = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("напишите число :");
+int numB = Convert.ToInt32(Console.ReadLine()); 
+
+
+
+    for(int i=number; i <=number;  i++)
+{
+   
+    Console.Write(" ");
+    System.Console.Write($"Введенно в натуральную степень: {Math.Pow(i,numB)}");     
+}
+*/
+
+// // Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// // 452 -> 11
+// // 82 -> 10
+// // 9012 -> 12
+
+/*
+SumNumbers(number, len);
+{
+    
+int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
+
+// System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма:  {Sum}");
+// {
+   
+//     Console.Write(" ");
+//     System.Console.Write($"Введенно в натуральную степень: {Math.Pow(i,numB)}");     
+// }
+
+*/
+
+
+// // Задача 29: Напишите программу, которая задаёт массив из 8 
+// элементов и выводит их на экран.
+// // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// // 6, 1, 33 -> [6, 1, 33]
+/*
+Console.WriteLine("ebashim");
+Console.Write("Введите массив: ");
+int enterNumber = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[enterNumber];
+
+for    (int i=0; i < enterNumber; i++) 
+{
+array[i] = new Random().Next(enterNumber+1);
+Console.Write(array[i] + "; ");
+}
+Console.WriteLine();
+
+//Вариант вывода вне цикла:
+//Console.WriteLine($"[{String.Join("; ", array)}]");
+*/
+
+
+
+
