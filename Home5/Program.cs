22dfff71eef29c514fbcf0ca4ea0a9bbe863e403
yankeeq <125729776+yankeeq@users.noreply.github.com>
2023-03-19 20:37:27 +0300

    38 задача

--- Home5/Program.cs
+++ Home5/Program.cs
@@ -1,53 +1,36 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
-int[] FillArrayWhithRandomNumber(int size)
+/*
+Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[a];
+
+void mas(int a)
 {
-int[] arr = new int[size];
-Random rnd = new Random();
-for(int i = 0; i < arr.Length; i++)
+for (int i = 0; i < a; i++)
 {
-    if (number >=100 && number <1000)
-    arr[i] = rnd.Next(-9, 10);
-}
-return arr;
+randomArray[i] = new Random().Next(100,1000);
+
 }
 
-int size = Convert.ToInt32(Console.ReadLine());
-int[] array = FillArrayWhithRandomNumber(size);
-int search = Convert.ToInt32(Console.ReadLine());
-
-string result = "No";
-
-for(int i=0; i<array.Length; i++){
-if(array[i] == search){
-result = "yes";
-break;
 }
+
+int kol(int[] randomArray)
+{
+int kol = 0;
+for (int i = 0; i < randomArray.Length; i++)
+{
+if (randomArray[i] % 2 == 0)
+kol = kol + 1;
 }
-System.Console.WriteLine(result);
-// int Prompt(string ones1)
-// {
+return kol;
+}
 
-// System.Console.WriteLine($"{ones1}");
-// return Convert.ToInt32(Console.ReadLine());
-// }
-//     int number = Prompt("Введите трехзначное число");
-//     if (number >=100 && number <1000)
-// {
-//     int ones1 = (number / 10)%10;
-    
-// System.Console.WriteLine($"Вторая цифра числа {number} является {ones1}");
-// }
-//     else
-// System.Console.WriteLine("Это число не трехзначное");
-
-// int n = Convert.ToInt32(Console.ReadLine());
-// for(int i=1;i<=n;i++)
-// {
-// if (i % 2 == 0)
-// System.Console.Write(i + ", ");
-// }
+mas(a);
+System.Console.WriteLine($"[{string.Join(", ", randomArray)}]");
+Console.Write($"\nКоличество чётных чисел в массиве: {kol(randomArray)}");
+*/
 
 
 
@@ -55,9 +38,80 @@ System.Console.WriteLine(result);
 // элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+/*
+Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[a];
+DateTime dt = DateTime.Now;  ///время работы
+
+void mas(int a)
+{
+   
+for (int i = 0; i < a; i++)
+{
+randomArray[i] = new Random().Next(1,10);
+}
+}
+int kol(int[] randomArray)
+{
+int sum = 0;
+int i = 0;
+while (i < randomArray.Length)
+{
+sum = sum + randomArray[i];
+i = i + 2;
+}
+return sum;
+}
+Console.WriteLine((DateTime.Now - dt).TotalMilliseconds); ///время работы
+mas(a);
+System.Console.WriteLine($"[{string.Join(", ", randomArray)}]");
+Console.Write($"\nКоличество нечётных чисел в массиве: {kol(randomArray)}");
+*/
 
 
 
 // Задача 38: Задайте массив вещественных чисел(тип double). Найдите разницу между 
 // максимальным и минимальным элементов массива.
 // [3,1; 7,2; 22,3; 2,4; 78,5] -> 76,1
+
+// double[] array = new double[n];
+// Random rand = new Random();
+// for (int i = 0; i < array.Length; i++)
+// array[i] = rand.Next(1, 100) + rand.NextDouble();
+
+/*
+Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+double[] randomArray = new double[a];
+
+void mas(int a)
+{
+Random rand = new Random();
+for (int i = 0; i < a; i++)
+{
+randomArray[i] = rand.NextDouble();
+Console.Write($"{randomArray[i]:F2} ");
+}
+
+}
+
+double raz(double[] randomArray)
+{
+double min = randomArray[0];
+double max = randomArray[0];
+int i = 1;
+while (i < randomArray.Length)
+{
+if (max<randomArray[i])
+max = randomArray[i];
+if (min>randomArray[i])
+min = randomArray[i];
+i = i + 1;
+}
+return max-min;
+}
+
+mas(a);
+Console.Write($"\nРазница между максимальным и минимальным элементов массива: {raz(randomArray):F2}");
+*/
