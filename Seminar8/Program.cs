b4de6c183cd2aa3eb265b4d77dce1a53dcb303e3
yankeeq <125729776+yankeeq@users.noreply.github.com>
2023-03-31 17:53:27 +0300

    Seminar8

---
+++ Seminar8/Program.cs
@@ -0,0 +1,225 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, 
+//которая поменяет местами первую и последнюю строку массива.
+/*
+int[,] FillMatrixWithRandom(int row, int column)
+{
+int[,] matrix = new int[row, column];
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = rnd.Next(0, 10);
+}
+}
+return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+Console.Write($"{matrix[i, j]} \t");
+}
+Console.WriteLine();
+}
+}
+
+int[,] SwitchRows(int [,] matrix)
+{
+int temp = 0;
+
+for(int i = 0; i < matrix.GetLength(1); i++)
+{
+    temp = matrix[0, i];
+    matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+    matrix[matrix.GetLength(0) - 1, i] = temp;
+}
+return matrix;
+}
+
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+SwitchRows(matrix);
+PrintMatrix(matrix);
+*/
+
+// Задача 55: Задайте двумерный массив.
+//Напишите программу, которая заменяет строки на столбцы.
+//В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+/*
+int[,] FillMatrixWithRandom(int row, int column)
+{
+int[,] array = new int[row, column];
+Random rnd = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+array[i, j] = rnd.Next(0, 10);
+}
+}
+return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+Console.Write($"{array[i, j]} \t");
+}
+Console.WriteLine();
+}
+}
+
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+int[,] resultMatrix = new int[column, row];
+for (int i = 0; i < resultMatrix.GetLength(0); i++)
+{
+for (int j = 0; j < resultMatrix.GetLength(1); j++)
+{
+resultMatrix[i,j] = matrix[j,i];
+}
+}
+System.Console.WriteLine();
+PrintMatrix(resultMatrix);
+*/
+
+
+
+
+
+// Задача 57: Составить частотный словарь элементов двумерного массива.
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// 1, 2, 3
+// 4, 6, 1
+// 2, 1, 6
+// 1 встречается 3 раза
+// 2 встречается 2 раз
+// 3 встречается 1 раз
+// 4 встречается 1 раз
+// 6 встречается 2 раза
+
+// Первый вариант решения
+/*
+int[,] FillMatrixWithRandom(int row, int column)
+{
+int[,] array = new int[row, column];
+Random rnd = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+array[i, j] = rnd.Next(0, 10);
+}
+}
+return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+Console.Write($"{array[i, j]} \t");
+}
+Console.WriteLine();
+}
+}
+
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+int count = 0;
+
+for (int k = 0; k < 10; k++)
+{
+count = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+if(matrix[i,j] == k)
+{
+count++;
+}
+}
+}
+if(count > 0)
+{
+System.Console.WriteLine($"Кол-во повторений для {k} = {count} раз");
+}
+}
+
+Второй вариант решения
+
+int[,] FillMatrixWithRandom(int row, int column)
+{
+int[,] array = new int[row, column];
+Random rnd = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+array[i, j] = rnd.Next(0, 10);
+}
+}
+return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+Console.Write($"{array[i, j]} \t");
+}
+Console.WriteLine();
+}
+}
+
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+
+int[] countArray = new int[10];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+countArray[matrix[i, j]]++;
+}
+}
+
+for(int i = 0; i <countArray.Length; i++)
+{
+if(countArray[i] != 0)
+{
+System.Console.WriteLine($"Кол-во повторений для {i} = {countArray[i]} раз");
+}
+}
+*/
